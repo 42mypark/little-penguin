d8c3c24c1cb04b0694032f78953d47d394af89ce
42mypark <skyttl34@github.com>
2023-01-12 13:43:40 +0900

    feat: license, Kbuild

--- assignment01/hello.c
+++ assignment01/hello.c
@@ -1,15 +1,18 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("MYPARK");
 
 int init_module(void) {
 	printk(KERN_INFO "Hello world !\n");
 	return 0;
 }
 
-int cleanup_module(void) {
+void cleanup_module(void) {
 	printk("Cleanning up module.\n");
 }
 
-module_init(init_module);
-module_exit(cleanup_module);
+//module_init(init_module);
+//module_exit(cleanup_module);
 
